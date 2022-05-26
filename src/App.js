91d880b11333d76a6c8e5db2d192250faac0e0b4
Pham Huu Hien <phhien203@gmail.com>
2022-05-26 19:18:35 +0700

    Display the menu

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './styles/ice-cream.scss';
 import Header from './structure/Header';
 import Footer from './structure/Footer';
+import Menu from "./ice-cream/Menu";
 
 function App() {
   return (
     <>
       <Header />
+      <Menu />
       <Footer />
     </>
   );
---
+++ src/ice-cream/Menu.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { Helmet } from "react-helmet";
+import { getMenu } from '../data/iceCreamData';
+
+const Menu = () => {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+    getMenu().then((menu) => {
+      isMounted && setMenu(menu);
+    });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return (
+    <main>
+      <Helmet>
+        <title>Rock you taste buds with one of these! | Ultimate Ice Cream</title>
+      </Helmet>
+      <h2 className="main-heading">Rock you taste buds with one of these!</h2>
+      {menu.length > 0 ? (
+        <ul className="container">
+          {menu.map(
+            ({ id, iceCream, price, description, inStock, quantity }) => {
+              return (
+                <li key={id.toString()}>
+                  <section className="card">
+                    <div className="image-container"></div>
+                    <div className="text-container">
+                      <h3>{iceCream.name}</h3>
+                      <div className="content card-content">
+                        <p className="price">{`$${price.toFixed(2)}`}</p>
+                        <p className={`stock${inStock ? '' : ' out'}`}>
+                          {inStock
+                            ? `${quantity} in stock`
+                            : 'Current out of stock'}
+                        </p>
+                        <p className="description">{description}</p>
+                      </div>
+                    </div>
+                  </section>
+                </li>
+              );
+            }
+          )}
+        </ul>
+      ) : (
+        <p>Your menu is empty. The sadness!!</p>
+      )}
+    </main>
+  );
+};
+
+export default Menu;
