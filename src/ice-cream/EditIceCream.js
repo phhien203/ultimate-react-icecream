2da7be1306fab638aa2e65ad1a1063022d80ae0a
Pham Huu Hien <phhien203@gmail.com>
2022-05-28 10:51:47 +0700

    Build menu item form layout

--- src/ice-cream/EditIceCream.js
+++ src/ice-cream/EditIceCream.js
@@ -3,9 +3,17 @@ import { Helmet } from 'react-helmet';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getMenuItem } from '../data/iceCreamData';
 import LoaderMessage from '../structure/LoaderMessage';
+import '../styles/form-spacer.scss';
+import IceCreamImage from './IceScreamImage';
 
 const EditIceCream = () => {
-  const [menuItem, setMenuItem] = useState({});
+  const [menuItem, setMenuItem] = useState({
+    price: '0.00',
+    inStock: true,
+    quantity: '0',
+    description: '',
+    iceCream: {},
+  });
   const [isLoading, setIsLoading] = useState(false);
   const { menuItemId } = useParams();
   const navigate = useNavigate();
@@ -35,6 +43,48 @@ const EditIceCream = () => {
         doneMessage="Ice cream loaded."
         isLoading={isLoading}
       />
+      {!isLoading && (
+        <div className="form-frame">
+          <div className="image-container">
+            <IceCreamImage iceCreamId={menuItem.iceCream.id} />
+          </div>
+          <div className="form-container">
+            <dl>
+              <dt>Name :</dt>
+              <dd>{menuItem.iceCream.name}</dd>
+            </dl>
+            <form>
+              <label>Description :</label>
+              <textarea name="description" rows="3"></textarea>
+
+              <label>In stock :</label>
+              <div className="checkbox-wrapper">
+                <input type="checkbox" name="inStock" />
+                <div className="checkbox-wrapper-checked"></div>
+              </div>
+
+              <label>Quantity :</label>
+              <select name="quantity">
+                <option value="0">0</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+                <option value="40">40</option>
+                <option value="50">50</option>
+              </select>
+
+              <label>Price :</label>
+              <input type="number" step="0.01" name="price" />
+
+              <div className="button-container">
+                <button className="ok" type="submit">
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
     </main>
   );
 };
