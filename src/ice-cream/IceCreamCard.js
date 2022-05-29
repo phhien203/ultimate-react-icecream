e26b439be7b9b21b71bef0ab5276c0dd05ddcbbc
Pham Huu Hien <phhien203@gmail.com>
2022-05-29 14:11:35 +0700

    Add IceCreamCard component

---
+++ src/ice-cream/IceCreamCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import IceCreamImage from './IceScreamImage';
+import FocusLink from '../structure/FocusLink';
+import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const IceCreamCard = ({ children, to, iceCreamId, heading }) => {
+  const navigate = useNavigate();
+
+  const onItemClickHandler = () => {
+    navigate(to, { focus: true });
+  };
+
+  return (
+    <section className="card" onClick={onItemClickHandler}>
+      <div className="image-container">
+        <IceCreamImage iceCreamId={iceCreamId} />
+      </div>
+      <div className="text-container">
+        <h3>
+          <FocusLink
+            to={to}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            {heading}
+          </FocusLink>
+        </h3>
+        {children}
+      </div>
+    </section>
+  );
+};
+
+IceCreamCard.propTypes = {
+  iceCreamId: PropTypes.number.isRequired,
+  heading: PropTypes.string.isRequired,
+  children: PropTypes.node,
+  to: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+      focus: PropTypes.bool,
+    }),
+  ]),
+};
+
+export default IceCreamCard;
--- src/ice-cream/Menu.js
+++ src/ice-cream/Menu.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { getMenu } from '../data/iceCreamData';
-import IceCreamImage from './IceScreamImage';
 import LoaderMessage from '../structure/LoaderMessage';
 import { useNavigate } from 'react-router-dom';
 import Main from '../structure/Main';
-import FocusLink from '../structure/FocusLink';
+import IceCreamCard from './IceCreamCard';
 
 const Menu = () => {
   const [menu, setMenu] = useState([]);
@@ -42,37 +41,21 @@ const Menu = () => {
             ({ id, iceCream, price, description, inStock, quantity }) => {
               return (
                 <li key={id.toString()}>
-                  <section
-                    className="card"
-                    onClick={() => {
-                      onItemClickHandler(`/menu-items/${id.toString()}`);
-                    }}
+                  <IceCreamCard
+                    heading={iceCream.name}
+                    iceCreamId={iceCream.id}
+                    to={`/menu-items/${id.toString()}`}
                   >
-                    <div className="image-container">
-                      <IceCreamImage iceCreamId={iceCream.id} />
+                    <div className="content card-content">
+                      <p className="price">{`$${price.toFixed(2)}`}</p>
+                      <p className={`stock${inStock ? '' : ' out'}`}>
+                        {inStock
+                          ? `${quantity} in stock`
+                          : 'Current out of stock'}
+                      </p>
+                      <p className="description">{description}</p>
                     </div>
-                    <div className="text-container">
-                      <h3>
-                        <FocusLink
-                          to={`/menu-items/${id.toString()}`}
-                          onClick={(e) => {
-                            e.stopPropagation();
-                          }}
-                        >
-                          {iceCream.name}
-                        </FocusLink>
-                      </h3>
-                      <div className="content card-content">
-                        <p className="price">{`$${price.toFixed(2)}`}</p>
-                        <p className={`stock${inStock ? '' : ' out'}`}>
-                          {inStock
-                            ? `${quantity} in stock`
-                            : 'Current out of stock'}
-                        </p>
-                        <p className="description">{description}</p>
-                      </div>
-                    </div>
-                  </section>
+                  </IceCreamCard>
                 </li>
               );
             }
